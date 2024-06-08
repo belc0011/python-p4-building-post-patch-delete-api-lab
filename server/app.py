0e08fe282d337c22a29d8f04732487ee75bcdc4d
Monica Garcia <monicairenegarcia@gmail.com>
2024-06-08 12:36:31 -0700

    complete building post patch delete api lab

--- server/app.py
+++ server/app.py
@@ -23,12 +23,38 @@ def bakeries():
     bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
     return make_response(  bakeries,   200  )
 
-@app.route('/bakeries/<int:id>')
+@app.route('/bakeries/<int:id>', methods=['GET', 'PATCH'])
 def bakery_by_id(id):
 
     bakery = Bakery.query.filter_by(id=id).first()
     bakery_serialized = bakery.to_dict()
-    return make_response ( bakery_serialized, 200  )
+
+    if request.method == 'GET':
+        return make_response ( bakery_serialized, 200  )
+    elif request.method == 'PATCH':
+        for attr in request.form:
+            setattr(bakery, attr, request.form.get(attr))
+        db.session.add(bakery)
+        db.session.commit()
+
+        bakery_dict = bakery.to_dict()
+        response = make_response(bakery_dict, 200)
+        return response
+
+@app.route('/baked_goods', methods=['GET', 'POST'])
+def baked_goods():
+    baked_goods = BakedGood.query.all()    
+    if request.method == 'GET':
+        pass
+    elif request.method == 'POST':
+        new_baked_good = BakedGood(
+            name = request.form.get("name"),
+            price=request.form.get("price")
+        )
+    db.session.add(new_baked_good)
+    db.session.commit()
+    response = make_response(new_baked_good.to_dict(), 201)
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
@@ -37,7 +63,23 @@ def baked_goods_by_price():
         bg.to_dict() for bg in baked_goods_by_price
     ]
     return make_response( baked_goods_by_price_serialized, 200  )
-   
+
+@app.route('/baked_goods/<int:id>', methods=['GET', 'DELETE'])
+def baked_goods_by_id(id):
+    baked_good = BakedGood.query.filter_by(id=id).first()
+    if request.method == 'GET':
+        pass
+    elif request.method == 'DELETE':
+        db.session.delete(baked_good)
+        db.session.commit()
+
+        response_body = {
+                "delete_successful": True,
+                "message": "Baked good deleted."
+            }
+
+    response = make_response(response_body, 200)
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
